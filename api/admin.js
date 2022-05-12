61091bbc1b85522465e997f1171b06eac9a8c126
badsk <amisurvivaldiamond@gmail.com>
2022-05-12 11:29:40 +0000

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -198,6 +198,97 @@ module.exports.load = async function(app, db) {
         }
     });
 
+    app.get("/addresources", async (req, res) => {
+        let theme = indexjs.get(req);
+    
+        if (!req.session.pterodactyl) return four0four(req, res, theme);
+        
+        let cacheaccount = await fetch(
+            settings.pterodactyl.domain + "/api/application/users/" + (await db.get("users-" + req.session.userinfo.id)) + "?include=servers",
+            {
+              method: "get",
+              headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${settings.pterodactyl.key}` }
+            }
+        );
+        if (await cacheaccount.statusText == "Not Found") return four0four(req, res, theme);
+        let cacheaccountinfo = JSON.parse(await cacheaccount.text());
+    
+        req.session.pterodactyl = cacheaccountinfo.attributes;
+        if (cacheaccountinfo.attributes.root_admin !== true) return four0four(req, res, theme);
+    
+        let failredirect = theme.settings.redirect.failedsetresources ? theme.settings.redirect.failedsetresources : "/";
+    
+        if (!req.query.id) return res.redirect(`${failredirect}?err=MISSINGID`);
+    
+        if (!(await db.get("users-" + req.query.id))) return res.redirect(`${failredirect}?err=INVALIDID`);
+    
+        let successredirect = theme.settings.redirect.setresources ? theme.settings.redirect.setresources : "/";
+    
+        if (req.query.ram || req.query.disk || req.query.cpu || req.query.servers) {
+            let ramstring = req.query.ram;
+            let diskstring = req.query.disk;
+            let cpustring = req.query.cpu;
+            let serversstring = req.query.servers;
+
+            let currentextra = await db.get("extra-" + req.query.id);
+            let extra;
+
+            if (typeof currentextra == "object") {
+                extra = currentextra;
+            } else {
+                extra = {
+                    ram: 0,
+                    disk: 0,
+                    cpu: 0,
+                    servers: 0
+                }
+            }
+
+            if (ramstring) {
+                let ram = parseFloat(ramstring);
+                if (ram < 0 || ram > 999999999999999) {
+                    return res.redirect(`${failredirect}?err=RAMSIZE`);
+                }
+                extra.ram = extra.ram + ram;
+            }
+            
+            if (diskstring) {
+                let disk = parseFloat(diskstring);
+                if (disk < 0 || disk > 999999999999999) {
+                    return res.redirect(`${failredirect}?err=DISKSIZE`);
+                }
+                extra.disk = extra.disk + disk;
+            }
+            
+            if (cpustring) {
+                let cpu = parseFloat(cpustring);
+                if (cpu < 0 || cpu > 999999999999999) {
+                    return res.redirect(`${failredirect}?err=CPUSIZE`);
+                }
+                extra.cpu = extra.cpu + cpu;
+            }
+
+            if (serversstring) {
+                let servers = parseFloat(serversstring);
+                if (servers < 0 || servers > 999999999999999) {
+                    return res.redirect(`${failredirect}?err=SERVERSIZE`);
+                }
+                extra.servers = extra.servers + servers;
+            }
+            
+            if (extra.ram == 0 && extra.disk == 0 && extra.cpu == 0 && extra.servers == 0) {
+                await db.delete("extra-" + req.query.id);
+            } else {
+                await db.set("extra-" + req.query.id, extra);
+            }
+
+            adminjs.suspend(req.query.id);
+            return res.redirect(successredirect + "?err=none");
+        } else {
+            res.redirect(`${failredirect}?err=MISSINGVARIABLES`);
+        }
+    });
+
 
     app.get("/setplan", async (req, res) => {
         let theme = indexjs.get(req);
@@ -265,9 +356,9 @@ module.exports.load = async function(app, db) {
         if (!code.match(/^[a-z0-9]+$/i)) return res.redirect(theme.settings.redirect.couponcreationfailed + "?err=CREATECOUPONINVALIDCHARACTERS");
 
         let coins = req.query.coins || 0;
-        let ram = req.query.ram || 0;
-        let disk = req.query.disk || 0;
-        let cpu = req.query.cpu || 0;
+        let ram = req.query.ram * 1024 || 0;
+        let disk = req.query.disk * 1024 || 0;
+        let cpu = req.query.cpu * 100 || 0;
         let servers = req.query.servers || 0;
 
         coins = parseFloat(coins);
