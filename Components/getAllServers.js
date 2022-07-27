27b162efc8c35cf4e57f15ef755d80622c7d32f0
badsk <amisurvivaldiamond@gmail.com>
2022-07-27 10:28:19 +0000

    Update getAllServers.js

--- Components/getAllServers.js
+++ Components/getAllServers.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 const settings = require('../settings.json')
 
 module.exports = () => {
-        return (await fetch(
+        return (await.fetch(
                 settings.pterodactyl.domain + "/api/application/servers?per_page=1000000",
                 {
                     headers: {
